fac1c4629d7ea3b41ee176319b7621c81963982e
Shreya Halder <Shreya.Halder@go-mmt.com>
2023-01-11 11:07:26 +0530

    second commit

--- routes/routes.go
+++ routes/routes.go
@@ -12,10 +12,10 @@ func SetupRouter() *gin.Engine {
 	//grp1 := r.Group("/employee-api")
 	r.GET("/user", controllers.GetUsers)
 	r.POST("/user", controllers.CreateUser)
-	//grp1.GET("/employee", controllers.GetUsers)
-	//grp1.GET("employee/:id", controllers.GetUserByID)
-	//grp1.PUT("employee/:id", controllers.UpdateUser)
-	//grp1.DELETE("employee/:id", controllers.DeleteUser)
+	//grp1.GET("/user", controllers.GetUsers)
+	//grp1.GET("user/:id", controllers.GetUserByID)
+	//grp1.PUT("user/:id", controllers.UpdateUser)
+	//grp1.DELETE("user/:id", controllers.DeleteUser)
 
 	return r
 }
